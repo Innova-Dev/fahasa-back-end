7b1da712fc4d0177d897e3331aad54b45f03966e
kensine <tanndph25572@fpt.edu.vn>
2023-06-04 01:51:35 +0700

    SAH-30-fix-clean-backend-error

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,44 +1,46 @@
-import { signinSchema, signupSchema } from "../schemas/auth.js";
-import bcrypt from "bcryptjs";
-import User from "../models/user.js";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import { signinSchema, signupSchema } from '../schemas/auth.js';
+import bcrypt from 'bcryptjs';
+import User from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const { SECRET_CODE } = process.env;
 
 export const signup = async (req, res) => {
-  try {
-    const { error } = signupSchema.validate(req.body, { abortEarly: false });
-    const { email, password } = req.body;
-    if (error) {
-      return res.status(400).json({
-        message: error.details.map((err) => err.message),
-      });
-    }
+    try {
+        const { error } = signupSchema.validate(req.body, { abortEarly: false });
+        const { email, password } = req.body;
+        if (error) {
+            return res.status(400).json({
+                message: error.details.map((err) => err.message),
+            });
+        }
 
-    const haveEmail = await User.findOne({ email });
-    if (!haveEmail) {
-      return res.status(400).json({
-        message: "Email nay da dk dang ky",
-      });
-    }
+        const haveEmail = await User.findOne({ email });
+        if (haveEmail) {
+            return res.status(400).json({
+                message: 'Email nay da dk dang ky',
+            });
+        }
 
-    const hashPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({
-      ...req.body,
-      password: hashPassword,
-    });
-    user.password = undefined;
-    return res.status(201).json({
-      message: "Tạo tài khoản thành công",
-      user,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Loi server",
-    });
-  }
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            ...req.body,
+            password: hashPassword,
+        });
+        user.password = undefined;
+        return res.status(201).json({
+            status: true,
+            message: 'Tạo tài khoản thành công',
+            user,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: 'Loi server',
+        });
+    }
 };
 
 /**
@@ -50,43 +52,48 @@ export const signup = async (req, res) => {
  */
 
 export const signin = async (req, res) => {
-  try {
-    const { error } = signinSchema.validate(req.body, { abortEarly: false });
-    if (error) {
-      return res.status(400).json({
-        message: error.details.map((err) => err.message),
-      });
-    }
-    const { email, password } = req.body;
+    try {
+        const { error } = signinSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            return res.status(400).json({
+                status: false,
+                message: error.details.map((err) => err.message),
+            });
+        }
+        const { email, password } = req.body;
 
-    const haveUser = await User.findOne({ email });
-    if (!haveUser) {
-      return res.status(400).json({
-        message: "Tài khoản không tồn tại!",
-      });
+        const haveUser = await User.findOne({ email });
+        if (!haveUser) {
+            return res.status(400).json({
+                status: false,
+                message: 'Tài khoản không tồn tại!',
+            });
+        }
+        const checkPassword = await bcrypt.compare(password, haveUser.password);
+        if (!checkPassword) {
+            return res.status(400).json({
+                status: false,
+                message: 'Mật khẩu không khớp!',
+            });
+        }
+        const token = jwt.sign(
+            {
+                id: haveUser._id,
+            },
+            SECRET_CODE,
+            { expiresIn: '1d' },
+        );
+        haveUser.password = undefined;
+        return res.status(200).json({
+            status: true,
+            message: 'Đăng nhập thành công!',
+            accessToken: token,
+            user: haveUser,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: 'Loi server',
+        });
     }
-    const checkPassword = await bcrypt.compare(password, haveUser.password);
-    if (!checkPassword) {
-      return res.status(400).json({
-        message: "Mật khẩu không khớp!",
-      });
-    }
-    const token = jwt.sign(
-      {
-        id: haveUser._id,
-      },
-      SECRET_CODE,
-      { expiresIn: "1d" }
-    );
-    haveUser.password = undefined;
-    return res.status(200).json({
-      message: "Đăng nhập thành công!",
-      accessToken: token,
-      user: haveUser,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Loi server",
-    });
-  }
 };
