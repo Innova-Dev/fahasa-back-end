fcdb9d4ed6948f750c24e0fea79b6c0fa6afb86d
rynddev <manhnvph25559@fpt.edu.vn>
2023-06-11 23:16:28 +0700

    fix img khi up refs:SAH-53

--- src/schemas/product.js
+++ src/schemas/product.js
@@ -3,10 +3,7 @@ const productSchema = Joi.object({
   name: Joi.string(),
   list_price: Joi.number(),
   original_price:Joi.number(),
-  images: Joi.array().items(Joi.object({
-    type: Joi.string(),
-    url: Joi.string()
-  })),
+  images: Joi.array(),
   description: Joi.string(),
 
 });
